4acdb825e83945843014060e17edd6d6a2036444
sveta.sidorowa@gmail.com <123@gmail.com>
2025-10-13 12:22:38 -0700

    Added docs to square.py

--- square.py
+++ square.py
@@ -1,7 +1,11 @@
+import math
+""" импортируем библиотеку math для использования math.pi """
 
-def area(a):
-    return a * a
+def area(r):
+"""принимает число r, возращает площадь круга числа r"""
+    return math.pi * r * r
 
 
-def perimeter(a):
-    return 4 * a
+def perimeter(r):
+"""Принимает число r, возращает периметр круга числ r"""
+    return 2 * math.pi * r
